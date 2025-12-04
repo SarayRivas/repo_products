cab60e211f6b0db7516db602029013cda104b955
Saray Rivas <saraysahar.r@gmail.com>
2025-12-04 18:34:31 -0500

    .

--- inventories/views.py
+++ inventories/views.py
@@ -2,9 +2,20 @@
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by('id_product')
     serializer_class = ProductSerializer
 
+def _no_store(response):
+    response['Cache-Control'] = 'no-store'
+    return response
+
+@require_http_methods(["GET", "HEAD"])
+def health_check(request):
+
+    res = JsonResponse({"status": "ok"})
+    return _no_store(res)
